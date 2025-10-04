527539e65af8b64424474c551d1d1462686b6424
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-04 16:10:11 -0300

    Create 2_funcoes_uteis.py

---
+++ 2_numpy/2_funcoes_uteis.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1,2,3,4])
+
+print(np.sum(a)) # Soma total
+print(np.mean(a)) # Média
+print(np.std(a)) # Desvio padrão
+print(np.max(a)) # Máximo
+print(np.min(a)) # Mínimo
