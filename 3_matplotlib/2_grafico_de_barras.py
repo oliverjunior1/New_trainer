ccac468bcbc9fa0c06869e68b2e4564e6e34f593
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-04 22:45:53 -0300

    Create 2_grafico_de_barras.py

---
+++ 3_matplotlib/2_grafico_de_barras.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+letters = ['a', 'b', 'c', 'd', 'e']
+numbers = [10,5,28,30,15]
+
+plt.bar(letters, numbers, color='skyblue')
+plt.title("Gráfico de Barras")
+plt.show()
