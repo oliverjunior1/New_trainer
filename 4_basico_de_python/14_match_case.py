95b7fed48aaf405bc844d96be563ca1306b3f887
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-05 23:26:30 -0300

    Create 14_match_case.py

---
+++ 4_basico_de_python/14_match_case.py
@@ -0,0 +1,22 @@
+# about the days of the week with input
+from pygame.surfarray import pixels_red
+
+day = int(input("Put the number to see the day: "))
+
+match day:
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print('Tuesday')
+    case 4:
+        print('Wednesday')
+    case 5:
+        print('Thursday')
+    case 6:
+        print('Friday')
+    case 7:
+        print('Saturday')
+    case _:
+        print('Invalid number.')
