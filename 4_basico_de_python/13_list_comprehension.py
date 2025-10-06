8f1fe55103fb46fda881af3c5ad157e5dcf7bd60
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-05 23:26:25 -0300

    Create 13_list_comprehension.py

---
+++ 4_basico_de_python/13_list_comprehension.py
@@ -0,0 +1,15 @@
+
+# problem 1: Make a list with some family members, that change to a list with name and last name.
+
+name = ['Joaquim', 'Alyne', 'Joao', 'Mariane']
+
+name_lname = [x+" Oliveira" for x in name]
+
+print(name_lname)
+
+
+
+
+
+
+
