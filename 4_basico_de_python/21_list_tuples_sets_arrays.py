ff9f8f8b02e365777d828e33acaab974fd3bcb94
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-05 23:27:11 -0300

    Create 21_list_tuples_sets_arrays.py

---
+++ 4_basico_de_python/21_list_tuples_sets_arrays.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+my_list = [1,2,3]
+print(type(my_list))
+
+print("###################")
+
+my_tuple = (1,2,3)
+print(type(my_tuple))
+
+print("###################")
+
+my_dict = {'a': 1, 'b':2}
+print(type(my_dict))
+
+print("###################")
+
+my_set = {1,2,3}
+print(type(my_set))
+
+print("###################")
+
+my_array = pd.array([1,2,3])
+print(type(my_array))
+
+print("###################")
