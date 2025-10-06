3049d44d1fc668ad4516cf57713488dc893e32e7
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-05 23:27:45 -0300

    Create 3_new_match_mode.py

---
+++ 4_basico_de_python/3_new_match_mode.py
@@ -0,0 +1,21 @@
+while True:
+    day = int(input("Put the number to see the day: "))
+    match day:
+        case 1:
+            print("Sunday")
+        case 2:
+            print("Monday")
+        case 3:
+            print("Tuesday")
+        case 4:
+            print("Wednesday")
+        case 5:
+            print("Thursday")
+        case 6:
+            print("Friday")
+        case 7:
+            print("Saturday")
+        case 8:
+            break
+        case _:
+            print("Invalid number")
