ad5a7bd909790faeb53ead2992b3f0fa30c6bb51
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-05 23:28:32 -0300

    Create 9_classes.py

---
+++ 4_basico_de_python/9_classes.py
@@ -0,0 +1,15 @@
+# family
+
+class Family:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"The name is {self.name} and the age is {self.age} years old."
+
+son = Family("Joao", 12)
+daughter = Family("Mariane", 3)
+
+print(son)
+print(daughter)
