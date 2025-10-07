ec10c2a064043ad3b359c49023f13671bb190418
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-07 17:12:08 -0300

    Update 3_entry.py

--- 5_custom_tkinter/3_entry.py
+++ 5_custom_tkinter/3_entry.py
@@ -4,6 +4,6 @@ app = ctk.CTk()
 
 entry = ctk.CTkEntry(app, placeholder_text="Digite seu nome: ")
 entry.pack()
-button = ctk.CTkButton(app, text='Enviar', command=lambda: print(entry.get()))
+button = ctk.CTkButton(app, text='Enviar', command=lambda: print(f"Hi, {entry.get()}!"))
 button.pack()
 app.mainloop()
