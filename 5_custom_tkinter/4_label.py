2a6d4ac74a7f634a168f79f33f21940be4cdcaa8
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-06 22:58:21 -0300

    Update 4_label.py

--- 5_custom_tkinter/4_label.py
+++ 5_custom_tkinter/4_label.py
@@ -3,8 +3,7 @@ import customtkinter as ctk
 app = ctk.CTk()
 
 def change():
-    label = ctk.CTkLabel(text="Jesus How I love you!")
-    return label
+    ctk.CTkLabel(text="Jesus How I love you!")
 
 label = ctk.CTkLabel(app, text="Jesus, I love you so much!")
 label.pack()
