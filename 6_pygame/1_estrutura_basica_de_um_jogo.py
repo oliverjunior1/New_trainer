d05bdc05da89e68049e6d1661bc3aabc729dea87
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-08 18:32:11 -0300

    Update 1_estrutura_basica_de_um_jogo.py

--- 6_pygame/1_estrutura_basica_de_um_jogo.py
+++ 6_pygame/1_estrutura_basica_de_um_jogo.py
@@ -3,6 +3,13 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((600,600))
+pygame.display.set_caption("Meu primeiro jogo")
 
+running = True
+while running:
+    for evento in pygame.event.get():
+        if evento.type==pygame.QUIT:
+            running = False
+    screen.fill((0,0,0))
 
 pygame.quit()
