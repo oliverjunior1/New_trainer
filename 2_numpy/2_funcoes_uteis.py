e0ef0d2db19e521d6f4e55ee37997a828450ebcc
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-14 19:57:27 -0300

    Update 2_funcoes_uteis.py

--- 2_numpy/2_funcoes_uteis.py
+++ 2_numpy/2_funcoes_uteis.py
@@ -2,8 +2,8 @@ import numpy as np
 
 a = np.array([1,2,3,4])
 
-print(np.sum(a))
-print(np.mean(a))
-print(np.std(a))
-print(np.max(a))
-print(np.min(a))
+print(np.sum(a)) # Soma total
+print(np.mean(a)) # Média
+print(np.std(a)) # Desvio padrão
+print(np.max(a)) # Máximo
+print(np.min(a)) # Mínimo
