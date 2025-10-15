acb49a3fb4355af7fbb868cb0173fdff3b728a7a
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-15 09:05:35 -0300

    Update 1_line_plot.py

--- 3_matplotlib/1_line_plot.py
+++ 3_matplotlib/1_line_plot.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
 
 a = [1,2,3,4,5]
-b =
+b =[15,12,10,16,15]
+
+plt.plot(a, b)
+plt.title('Gráficos em Linha')
+plt.xlabel("Horas")
+plt.ylabel("Variações da temperatura")
+plt.show()
