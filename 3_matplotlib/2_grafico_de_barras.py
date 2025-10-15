2003e7509e9004c67aea1ac95a5d829ec59a8cdf
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-14 22:22:48 -0300

    Update 2_grafico_de_barras.py

--- 3_matplotlib/2_grafico_de_barras.py
+++ 3_matplotlib/2_grafico_de_barras.py
@@ -4,8 +4,8 @@ temp = [19, 15, 18, 22, 28, 32]
 horas = [4,5,8,10,12,15]
 
 plt.plot(horas, temp)
-plt.xlabel("Temperatura em ºC")
-plt.ylabel("Horas do dia")
-plt.title("Horas do dia x Temperatura")
+plt.xlabel("Temperatura em ºC", color='red')
+plt.ylabel("Horas do dia", color='blue')
+plt.title("Horas do dia x Temperatura", color="skyblue")
 
 plt.show()
