5df0af835054bd34f3be8a4c13682a9eddaff787
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-15 09:18:44 -0300

    Update 2_grafico_de_barras.py

--- 3_matplotlib/2_grafico_de_barras.py
+++ 3_matplotlib/2_grafico_de_barras.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
-meses = ['janeiro', 'março', 'maio', 'julho', 'agosto']
-chuvas = [100,80,40,70,150]
+meses = ['janeiro', 'abril', 'julho', 'outubro', 'dezembro']
+temperatura_media = [15,20,22,30,25]
 
-plt.title("Chuva por época do ano:", color='blue')
-plt.bar(meses, chuvas, color='pink')
-plt.xlabel('Meses do ano', color= 'green')
-plt.ylabel('ml/m² de chuva', color="red")
+plt.bar(meses, temperatura_media, color='lightblue')
+plt.title("Temperatura Media Durante os Meses do Ano", color='blue')
+plt.xlabel('Meses')
+plt.ylabel('Temperatura')
+
 
 plt.show()
