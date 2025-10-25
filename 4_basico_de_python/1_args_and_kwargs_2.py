9eaee8c5420c3c31af5402592f11b682643d7cea
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-25 20:02:49 -0300

    Update 1_args_and_kwargs_2.py

--- 4_basico_de_python/1_args_and_kwargs_2.py
+++ 4_basico_de_python/1_args_and_kwargs_2.py
@@ -1,2 +1,11 @@
+def args_sum(*args):
+    print(sum(args))
 
+# Solicita os números separados por espaço
+x = input("Put the numbers, to sum: ")
 
+# Converte a string em uma lista de inteiros
+nums = list(map(int, x.split(',')))
+
+# Chama a função com os números como argumentos
+args_sum(*nums)
