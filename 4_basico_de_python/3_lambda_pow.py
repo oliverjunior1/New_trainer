4bc2384f3fa5eaa68669f3eba894c23c94fe0563
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-26 22:28:53 -0300

    Update 3_lambda_pow.py

--- 4_basico_de_python/3_lambda_pow.py
+++ 4_basico_de_python/3_lambda_pow.py
@@ -1,4 +1,12 @@
+while True:
+    number1 = int(input("Put the first number: "))
+    number2 = int(input("Put the second number: "))
+    number3 = int(input("Put the third number: "))
 
+    print(f"The math case is ({number1}-{number2}){number3}. ")
+
+    calculate = lambda a, b, c: (a-b)**c
+    print(calculate(number1, number2, number3))
 
 
 
