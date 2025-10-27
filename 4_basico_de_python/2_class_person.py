999ca5d65151dfa289e7b6d797042879702859e3
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-26 22:15:24 -0300

    Update 2_class_person.py

--- 4_basico_de_python/2_class_person.py
+++ 4_basico_de_python/2_class_person.py
@@ -1,1 +1,14 @@
 # class big bang
+class big_bang:
+    def __init__(self, character, comic_books):
+        self.character = character
+        self.comic_books = comic_books
+
+    def __str__(self):
+        return f"The character {self.character} has {self.comic_books} comic books in his bedroom."
+
+Sheldon = big_bang("Sheldon", 25000)
+Leonard = big_bang("Leonard", 19985)
+
+print(Sheldon)
+print(Leonard)
