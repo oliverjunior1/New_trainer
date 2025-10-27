0363ea3dcb77965ef22981b3d11f918f8dac04b5
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-26 22:15:55 -0300

    Update 3_lambda_pow.py

--- 4_basico_de_python/3_lambda_pow.py
+++ 4_basico_de_python/3_lambda_pow.py
@@ -1,11 +1,4 @@
-while True:
-    a = float(input("Put a number: "))
-    b = float(input("Put another number: "))
-    c = float(input("Put other number: "))
 
-    calculo = lambda x,y,z: (x**y)**z
-
-    print(calculo(a, b, c))
 
 
 
