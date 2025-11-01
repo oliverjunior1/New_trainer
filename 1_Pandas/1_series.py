12c23778007f957211ce9a8fde7bd3c1d592c9fc
oliverjunior1 <oliver-junior@hotmail.com>
2025-11-01 14:22:29 -0300

    Update 1_series.py

--- 1_Pandas/1_series.py
+++ 1_Pandas/1_series.py
@@ -1,11 +1,1 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-x = pd.Series([15,17,25,18,17,26])
-
-plt.plot(x)
-plt.title("TEMPERATURA/DIAS")
-plt.ylabel('temperatura')
-plt.xlabel('dias')
-plt.show()
 
