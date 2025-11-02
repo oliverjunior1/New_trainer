aeae724aaddff9b152b6ed9ca0cb14385a635c5f
oliverjunior1 <oliver-junior@hotmail.com>
2025-11-02 16:06:47 -0300

    Update 3_exercicio_idades.py

--- 2_numpy/3_exercicio_idades.py
+++ 2_numpy/3_exercicio_idades.py
@@ -5,19 +5,24 @@ import numpy as np
 # - Crie um array com o dobro das idades.
 
 idade = [42,4,13,39,50,73,49,9,12,17]
+idade_acima_de_30 = []
 
-media = np.mean(idade)
-print(f"A média das idades é: {media}")
-
-def filtro_idade():
-    for i in idade:
-        if i>=30:
-            print(f"A pessoa com {i} anos tem mais de 30.")
+def media():
+    media = np.mean(idade)
+    print(f'A média das idades é: {media}')
+def age_up_to_30():
+    for age in idade:
+        if age>30:
+            idade_acima_de_30.append(age)
         else:
             pass
+    print(f'As idades acima dos 30, são: {idade_acima_de_30}')
 
-filtro_idade()
+def dobrar_idades():
+    twice_ages = [x*2 for x in idade]
+    print(f"O dobro das idades é: {twice_ages}")
 
-dobro_das_idades = [x*2 for x in idade]
+media()
+age_up_to_30()
+dobrar_idades()
 
-print(f"O dobro das idades é: {dobro_das_idades}")
